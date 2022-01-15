5bf945cd133b4c5dfed383ab1dabaae46e0793d2
cristian.orozco <cristian.orozco@globant.com>
2022-01-15 15:34:00 -0500

    3d portfolio with threeJS

---
+++ main.js
@@ -0,0 +1,104 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+const scene = new THREE.Scene()
+
+// const camera = new THREE.PerspectiveCamera(field of view, aspect ratio, view frustum )
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+})
+
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setSize(window.innerWidth, window.innerHeight)
+camera.position.setZ(30)
+renderer.render(scene, camera)
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347 })
+const torus = new THREE.Mesh(geometry, material)
+scene.add(torus)
+
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(5, 5, 5)
+const ambientLight = new THREE.AmbientLight(0xffffff)
+scene.add(pointLight, ambientLight)
+
+// Three.js helpers
+const lighHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50)
+scene.add(lighHelper, gridHelper)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+
+// Generate Random Stars
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24)
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff })
+  const star = new THREE.Mesh(geometry, material)
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100))
+  star.position.set(x, y, z)
+  scene.add(star)
+}
+
+Array(200).fill().forEach(addStar)
+
+// Background
+const spaceTexture = new THREE.TextureLoader().load('./assets/space.jpg')
+scene.background = spaceTexture
+
+// Texture Mapping in cube profile picture
+const crissTexture = new THREE.TextureLoader().load('./assets/jeff.png')
+const criss = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: crissTexture }))
+scene.add(criss)
+
+// Moon
+const moonTexture = new THREE.TextureLoader().load('./assets/moon.jpg')
+const normalTexture = new THREE.TextureLoader().load('./assets/normal.jpg')
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshBasicMaterial({
+    map: moonTexture,
+    normalMap: normalTexture,
+  })
+)
+scene.add(moon)
+
+moon.position.z = 30
+moon.position.setX(-10)
+
+criss.position.z = -5
+criss.position.x = 2
+
+// Scroll Animation
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top
+  moon.rotation.x += 0.05
+  moon.rotation.y += 0.075
+  moon.rotation.z += 0.05
+
+  criss.rotation.y += 0.01
+  criss.rotation.z += 0.01
+
+  camera.position.z = t * -0.01
+  camera.position.x = t * -0.0002
+  camera.position.y = t * -0.0002
+}
+document.body.onscroll = moveCamera
+
+function animate() {
+  requestAnimationFrame(animate)
+  torus.rotation.x += 0.01
+  torus.rotation.y += 0.005
+  torus.rotation.z += 0.01
+
+  // controls.update()
+  renderer.render(scene, camera)
+}
+
+animate()
